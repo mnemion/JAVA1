7adb8ec41f8f5489403e3f0f1974939f0d13e3fd
mnemion <game1761@naver.com>
2023-05-03 17:28:31 +0900

    first commit

---
+++ chapter03/src/sec02/exam06/StringConcatExample.java
@@ -0,0 +1,11 @@
+package sec02.exam06;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		String str1 = "JDK" + 6.0;
+		String str2 = "JDK" + 6.0;
+		System.out.println("str1: " + str1);
+	}
+
+}
