34009b72d6f31b8edf60c52a04f98b1474f93db5
mnemion <game1761@naver.com>
2023-05-11 18:11:18 +0900

    first commit

---
+++ chapter05/src/sec03/exam01/Week.java
@@ -0,0 +1,11 @@
+package sec03.exam01;
+
+public enum Week {
+	MonDay,
+	TUEDAY,
+	WEDNESDAY,
+	TNURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
---
+++ chapter05/src/sec03/exam02/Week.java
@@ -0,0 +1,11 @@
+package sec03.exam02;
+
+public enum Week {
+	MonDay,
+	TUEDAY,
+	WEDNESDAY,
+	TNURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
