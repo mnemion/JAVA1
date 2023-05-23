0db510d63d56d916c023474120587d1328760614
mnemion <game1761@naver.com>
2023-05-23 14:48:59 +0900

    first commit

---
+++ chapter06/src/sec04/exam01/CalculatorExample.java
@@ -0,0 +1,23 @@
+package sec04.exam01;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator myCalc = new Calculator();
+		myCalc.powerOn();
+		
+		int result1 = myCalc.plus(5, 6);
+		System.out.println("result1: " + result1);
+		
+		byte x = 10;
+		byte y = 4;
+		double result2 = myCalc.divide(x, y);
+		double result3 = myCalc.divide(10.5, 20.5);
+		System.out.println("result2: " + result2);
+		System.out.println("result3: " + result3);
+		
+		myCalc.powerOff();
+	}
+
+}
