8da0db8b17cd1570e6b19113f940f7892cac2cc5
mnemion <game1761@naver.com>
2023-05-23 18:21:52 +0900

    first commit

--- chapter06/src/sec05/exam01/Car.java
+++ chapter06/src/sec05/exam01/Car.java
@@ -14,7 +14,7 @@ public class Car {
 	void run() {
 		for(int i=10; i<=50; i+=10) {
 			this.setSpeed(i);
-			System.out.println(this.model + "가 달립니다.(시속: " + this.speed + "km/h");
+			System.out.println(this.model + "가 달립니다. (시속: " + this.speed + "km/h)");
 		}
 	}
 }
--- chapter06/src/sec05/exam01/CarExample.java
+++ chapter06/src/sec05/exam01/CarExample.java
@@ -4,7 +4,11 @@ public class CarExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Car myCar = new Car("포르쉐");
+		Car myCar2 = new Car("벤츠");
+		
+		myCar.run();
+		myCar2.run();
 	}
 
 }
