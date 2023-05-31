e8dcef3ed74d3ae12711b8c14a469e1fded8de7d
mnemion <game1761@naver.com>
2023-05-31 18:01:23 +0900

    test

--- chapter07/src/sec02/exam04/Driver.java
+++ chapter07/src/sec02/exam04/Driver.java
@@ -1,6 +1,6 @@
 package sec02.exam04;
 
-public class Driver extends Vehicle {
+public class Driver extends Vehicle { // ver Vehicle vehicle = Bus bus 클래스 타입 / 변수명
 //		Driver driver = new Driver();
 //		Vehicle vehicle = new Vehicle();
 		
